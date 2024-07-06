ad65b9aeaa2607283971a26c135d1fc5a7b9da0b
tunahan <ttezcan.1999@gmail.com>
2024-07-06 19:11:28 +0300

    feat: Add delete and change status routes for todos
    New routes have been added to allow users to delete a todo and change its status. These routes are protected with authentication middleware to ensure only authenticated users can perform these actions.
    
    Related to recent user commits:
    - Remove Token model and related code
    - Update user routes to include /me endpoint
    - Update token expiration times for access and refresh tokens
    - Add initial project files and dependencies

--- routes/todo.js
+++ routes/todo.js
@@ -23,7 +23,22 @@ router.post('/', verify, async (req, res) => {
 });
 
 // delete Todo
-
+router.delete('/:id', verify, async (req, res) => {
+  const userId = req.user.id;
+  const todoId = req.params.id;
+  console.log(userId, todoId);
+  try {
+    const todo = await Todo.findOne({ userId: userId, _id: todoId });
+    if (todo) {
+      await todo.deleteOne();
+      res.status(200).json('Todo has been deleted...');
+    } else {
+      res.status(404).json('Todo not found...');
+    }
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 // get Todos
 router.get('/', verify, async (req, res) => {
   const userId = req.user.id;
@@ -31,4 +46,21 @@ router.get('/', verify, async (req, res) => {
   res.status(200).json(todos);
 });
 
+// change todo status
+router.patch('/:id', verify, async (req, res) => {
+  const userId = req.user.id;
+  const todoId = req.params.id;
+  try {
+    const todo = await Todo.findOne({ userId: userId, _id: todoId });
+    if (todo) {
+      todo.status = !todo.status;
+      await todo.save();
+      res.status(200).json(todo);
+    } else {
+      res.status(404).json('Todo not found...');
+    }
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 module.exports = router;
