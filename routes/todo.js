dad6d714921cd577cb71c2d5d1ba01d22f645f50
tunahan <ttezcan.1999@gmail.com>
2024-07-08 12:14:43 +0300

    refactor: Update user routes to include /me endpoint

--- routes/todo.js
+++ routes/todo.js
@@ -1,11 +1,23 @@
 const verify = require('../middlewares/auth');
 const router = require('express').Router();
 const Todo = require('../models/Todo');
-const User = require('../models/User');
-// create Todo
-router.post('/', verify, async (req, res) => {
-  console.log(req.user.id);
-  if (req.user.id) {
+const { query, validationResult, body } = require('express-validator');
+
+// POST /todo
+router.post(
+  '/',
+  verify,
+  [
+    body('title').notEmpty().withMessage('Title is required'),
+    body('desc').notEmpty().withMessage('Description is required'),
+    body('img').optional().isURL().withMessage('Image must be a valid URL'),
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
     const newTodo = new Todo({
       title: req.body.title,
       desc: req.body.desc,
@@ -13,6 +25,7 @@ router.post('/', verify, async (req, res) => {
       userId: req.user.id,
       status: false,
     });
+
     try {
       const todo = await newTodo.save();
       res.status(200).json(todo);
@@ -20,7 +33,7 @@ router.post('/', verify, async (req, res) => {
       res.status(500).json(err);
     }
   }
-});
+);
 
 // delete Todo
 router.delete('/:id', verify, async (req, res) => {
@@ -38,12 +51,6 @@ router.delete('/:id', verify, async (req, res) => {
   } catch (err) {
     res.status(500).json(err);
   }
-});
-// get Todos
-router.get('/', verify, async (req, res) => {
-  const userId = req.user.id;
-  const todos = await Todo.find({ userId: userId });
-  res.status(200).json(todos);
 });
 
 // change todo status
@@ -64,3 +71,47 @@ router.patch('/:id', verify, async (req, res) => {
   }
 });
 module.exports = router;
+
+// GET /todos?pages=1&limit=10
+router.get(
+  '/',
+  verify,
+  [
+    query('page').optional().isInt({ min: 1 }).withMessage('Page must be a positive integer'),
+    query('limit').optional().isInt({ min: 1 }).withMessage('Limit must be a positive integer'),
+    query('search').optional().isString().withMessage('Search must be a string'),
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    const userId = req.user.id;
+    const page = parseInt(req.query.page) || 1;
+    const limit = parseInt(req.query.limit) || 10;
+    const skip = (page - 1) * limit;
+    const search = req.query.search;
+
+    // Arama filtresi oluştur
+    let filter = { userId: userId };
+    if (search) {
+      filter.title = { $regex: search, $options: 'i' }; // 'i' seçeneği büyük/küçük harf duyarsız arama yapar
+    }
+
+    try {
+      const todos = await Todo.find(filter).skip(skip).limit(limit);
+
+      const totalTodos = await Todo.countDocuments(filter);
+      const totalPages = Math.ceil(totalTodos / limit);
+
+      res.status(200).json({
+        todos,
+        totalPages,
+        currentPage: page,
+      });
+    } catch (error) {
+      res.status(500).json({ error: 'Server error' });
+    }
+  }
+);
