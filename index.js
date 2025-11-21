3ceecc4c7aee1623522d154ac0dd70462301e645
tunahan <ttezcan.1999@gmail.com>
2025-11-21 23:13:31 +0300

    inital commit

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require("express");
+
+const PORT = 8000;
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Hello, World!");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
